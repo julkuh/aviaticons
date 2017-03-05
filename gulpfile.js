7e3d2e2ff0f2cd30f4979d78bd531b65ac197d9e
julkuh <julkuh@googlemail.com>
2017-03-05 06:49:35 +0100

    automated build of demo.html

--- gulpfile.js
+++ gulpfile.js
@@ -5,28 +5,42 @@ var del = require('del');
 var iconfont = require('gulp-iconfont');
 var consolidate = require('gulp-consolidate');
 
+// Global Config
+var config = {
+    fontName: 'aviaticons',
+    cssClassPrefix: 'ai',
+    formats: ['svg', 'ttf', 'eot', 'woff']
+}
+
 // Build the WebFont and copy it to dist Folder including the css needed.
 gulp.task('BuildFont', function (done) {
     var iconStream = gulp.src(['src/svg/*.svg'])
         .pipe(iconfont({
-            fontName: 'aviaticons',
-            formats: ['svg', 'ttf', 'eot', 'woff']
+            fontName: config.fontName,
+            formats: config.formats
         }));
 
     async.parallel([
     function handleGlyphs(cb) {
             iconStream.on('glyphs', function (glyphs, options) {
+                var replacementOptions = {
+                    glyphs: glyphs,
+                    fontPath: './',
+                    fontName: config.fontName,
+                    className: config.cssClassPrefix
+                };
+
                 gulp.src('src/css/aviaticons.css')
-                    .pipe(consolidate('lodash', {
-                        glyphs: glyphs,
-                        fontName: 'aviaticons',
-                        fontPath: './',
-                        className: 'ai'
-                    }))
-                    .pipe(gulp.dest('dist/aviaticons/font/'))
+                    .pipe(consolidate('lodash', replacementOptions))
+                    .pipe(gulp.dest('dist/aviaticons/font/'));
+
+                gulp.src('src/demo.html')
+                    .pipe(consolidate('lodash', replacementOptions))
+                    .pipe(gulp.dest('dist/'))
                     .on('finish', cb);
             });
     },
+
     function handleFonts(cb) {
             iconStream
                 .pipe(gulp.dest('dist/aviaticons/font/'))
@@ -37,8 +51,8 @@ gulp.task('BuildFont', function (done) {
 
 // Copy all demo related files into Dist
 gulp.task('BuildDemo', function () {
-    gulp.src(['./src/demo-files**/*', 'src/demo.html'])
-        .pipe(gulp.dest('./dist'));
+    gulp.src(['./src/demo-files/**/*'])
+        .pipe(gulp.dest('./dist/demo-files'));
 });
 
 //clean dest folder
@@ -47,8 +61,8 @@ gulp.task('clean', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch(['./src/demo.html', './src/demo-files/demo.css'], ['BuildDemo']);
-    gulp.watch(['./src/svg/**/*'], ['BuildFont']);
+    gulp.watch(['./src/demo-files/**/*'], ['BuildDemo']);
+    gulp.watch(['./src/svg/**/*', './src/demo.html'], ['BuildFont']);
 });
 
 gulp.task('build', function (cb) {
